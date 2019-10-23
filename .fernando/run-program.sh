a4038298d39f028c0f3fe3c3a71a434dd7d4a594
bachrc <8.bachrc@gmail.com>
2019-10-23 13:27:12 +0200

    Ajout du script de tests

--- .fernando/run-program.sh
+++ .fernando/run-program.sh
@@ -2,24 +2,38 @@
 
 PROBLEM_NUMBER=$1
 
-INPUT_FILE_REGEX="input(\d*)\.txt"
+INPUT_FILE_REGEX='input([[:digit:]]*)\.txt$'
 EPREUVE_FOLDER="./epreuve-$PROBLEM_NUMBER"
 
 for filename in "$EPREUVE_FOLDER"/input*.txt; do
-  echo "je me lance pour $filename"
+  echo "Lecture du fichier : $filename"
   if [[ $filename =~ $INPUT_FILE_REGEX ]]; then
     input_file_number="${BASH_REMATCH[1]}"
-    expected_output=$(cat "$EPREUVE_FOLDER/output$input_file_number.txt")
+    expected_output_file="$EPREUVE_FOLDER/output$input_file_number.txt"
 
+    if ! test -f "$expected_output_file"; then
+      echo "Erreur technique : fichier output correspondant à $filename introuvable." >&2
+      exit 1
+    fi
+
+    echo "Lecture du fichier : $expected_output_file."
+    expected_output=$(cat "$expected_output_file")
+
+    echo "Execution du programme en cours..."
     program_output=$(../mvnw compile test exec:java -Dexec.mainClass=fr.lacombe.fernandochase.Main -f ".." -Dexec.args="$filename" -q)
 
     if [ "$program_output" = "$expected_output" ]; then
-      echo "oui c'est bien"
+      echo "Le programme a bien retourné la valeur attendue !"
     else
-      echo "non c'est pas bien" >&2
+      echo "Le programme a retourné une valeur inattendue pour le test $input_file_number."
+      echo "Valeur attendue : $expected_output"
+      echo "Valeur renvoyée par le programme : $program_output"
+      echo "Epreuve non validée." >&2
+      exit 1
     fi
   else
-    echo "pas de fichier output correspondant au fichier input"
+    echo "Erreur technique : mauvais formattage du nom de $filename" >&2
+    exit 1
   fi
 
 done
